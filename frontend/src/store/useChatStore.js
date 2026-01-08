6b73f4521119c5eaa9a1d644941746fcca17c684
Pulkit Malhotra <pulkit@Pulkits-MacBook-Air.local>
2026-01-08 21:42:13 +0530

    added message system

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
 	messages: [],
 	users: [],
 	selectedUser: null,
@@ -30,6 +30,21 @@ export const useChatStore = create((set) => ({
 			toast.error(error.response.data.message);
 		} finally {
 			set({ isMessagesLoading: false });
+		}
+	},
+
+	sendMessage: async (messageData) => {
+		// we can't directly access variables and set it
+		// to get variable we need to have get function from zustand
+		const { messages, selectedUser } = get();
+		try {
+			const res = await axiosInstance.post(
+				`/messages/send/${selectedUser._id}`,
+				messageData
+			);
+			set({ messages: [...messages, res.data] });
+		} catch (error) {
+			toast.error(error.response.data.message);
 		}
 	},
 
