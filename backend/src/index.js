01a4f57f16c727cbab479ccf21d66e826a40dcf6
Pulkit Malhotra <pulkit@Pulkits-MacBook-Air.local>
2026-01-09 22:21:12 +0530

    express v5 all route

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
 
 	app.use(express.static(frontendPath));
 
-	app.get("*", (req, res) => {
+	app.use((req, res) => {
 		res.sendFile(path.join(frontendPath, "index.html"));
 	});
 }
